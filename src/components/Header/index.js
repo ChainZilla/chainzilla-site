ca95241bcfad268f1471be3774593374903c8e9e
PTYX11 <32718517+ptyx11@users.noreply.github.com>
2018-06-21 11:22:45 +0200

    grammar changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -27,9 +27,9 @@ export default () => <header className={styles.header}>
 	<div className={styles.texts}>
 		<p className={styles.inBanking}>In <span style={{fontWeight: 'bold'}}>BANKING</span>, math is based on trust.</p>
 		<p className={styles.inCrypto}>In <span style={{fontWeight: 'bold'}}>CRYPTO</span>, trust is based on math.</p>
-		<p className={styles.mission}>Chainzilla’s mission is to become the standard in development of DIY solutions, decentralized applications, and blockchain deployment.</p>
+		<p className={styles.mission}>ChainZilla’s mission is to become the standard in development of DIY solutions, decentralized applications, and blockchain deployment.</p>
 		<p className={styles.diy}>DIY Blockchain Kit- 4 Week Delivery</p>
-		<p className={styles.komodo}>Chainzilla is a 4th gen DIY blockchain service provider of Komodo Assetchains, Altcoins, and ERC20 Tokens. No programming needed!</p>
+		<p className={styles.komodo}>ChainZilla is a 4th gen DIY blockchain service provider of Komodo Assetchains, Altcoins, and ERC20 Tokens. No programming needed!</p>
 		<div onClick={() => window.scrollTo({ top: 3915, behavior: "smooth"}) } className={styles.launch}>
 			<p className={styles.launchText}>READY TO LAUNCH?</p>
 			<img style={{width: '25px'}} src={rocket} />
@@ -39,8 +39,8 @@ export default () => <header className={styles.header}>
 		<p className={styles.inBankingMobile} style={{textAlign: 'center', color: '#fff', fontWeight: '300'}}>In <span style={{fontWeight: 'bold'}}>BANKING</span>, math is based on trust.</p>
 		<p className={styles.inCryptoMobile} style={{textAlign: 'center', color: '#121329', fontWeight: '300'}}>In <span style={{fontWeight: 'bold'}}>CRYPTO</span>, trust is based on math.</p>
 		<p className={styles.diyMobile}>DIY Blockchain Kit - 4 Week Delivery</p>
-		<p className={styles.komodoMobile}>Chainzilla is a 4th gen DIY blockchainservice provider of Komodo Assetchains, Altcoins, and ERC20 Tokens. No programming needed!</p>
-		<p className={styles.komodoMobile}>Chainzilla’s mission is to become the standard in development of DIY solutions, decentralized applications, and blockchain deployment.</p>
+		<p className={styles.komodoMobile}>ChainZilla is a 4th gen DIY blockchainservice provider of Komodo Assetchains, Altcoins, and ERC20 Tokens. No programming needed!</p>
+		<p className={styles.komodoMobile}>ChainZilla’s mission is to become the standard in development of DIY solutions, decentralized applications, and blockchain deployment.</p>
 		<button className={styles.launchMobile}>READY TO LAUNCH?</button>
 	</div>
 	<div onClick={() => window.scrollTo({ top: 3915, behavior: "smooth"})} className={styles.circle}>
