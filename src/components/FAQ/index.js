e95b0f797eb90fcd4769a76e8d867e67f86f669c
PTYX11 <32718517+ptyx11@users.noreply.github.com>
2018-06-21 11:47:58 +0200

    grammer changes

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -16,14 +16,14 @@ export default () => <div className={styles.faq}>
 		<div className={styles.wrapper}>
 			<h2>Where can I go for help?</h2>
 			<p style={{fontWeight: '300', fontSize: '.7rem'}}>Click <a style={{color: 'white'}} href="http://support.chainzilla.io/a/solutions/"> HERE</a> to open a ticket and we will address your issue as soon as we can.</p>
-			<h2>Who can use Chainzilla?</h2>
+			<h2>Who can use ChainZilla?</h2>
 			<ul>
 				{
 					items.map(item => <li key={item}>{item}</li>)
 				}
 			</ul>
-			<h2>Does Chainzilla OFFER SPECIAL PARTNERSHIPS?</h2>
-			<p style={{fontWeight: '300', fontSize: '.8rem'}}>Yes, Chainzilla offers special partnerships to qualified projects and custom blockchain solutions. Our management consulting services focus on our clients' most critical issues and opportunities: strategy, technology, marketing, and community. We bring deep, functional expertise provided by highly skilled professionals from the blockchain space.</p>
+			<h2>Does ChainZilla OFFER SPECIAL PARTNERSHIPS?</h2>
+			<p style={{fontWeight: '300', fontSize: '.8rem'}}>Yes, ChainZilla offers special partnerships to qualified projects and custom blockchain solutions. Our management consulting services focus on our clients' most critical issues and opportunities: strategy, technology, marketing, and community. We bring deep, functional expertise provided by highly skilled professionals from the blockchain space.</p>
 		</div>
 	</div>
 </div>
