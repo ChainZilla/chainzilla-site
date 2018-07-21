9482a3ecefdfc60ed5d3346931fb197a7bcbb5b0
nourneme <40342712+nourneme@users.noreply.github.com>
2018-07-20 20:13:52 -0400

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -24,7 +24,7 @@ import icoicon from '../img/services_ICOicon-m.png'
 import kmdicon from '../img/services_KMDicon-m.png'
 import misc from '../img/services_MISCicon-m.png'
 import nem from '../img/services_NEMicon-m.png'
-import SPV from '../img/services_SPVicon-m.png'
+import spv from '../img/services_SPVicon-m.png'
 import wallet from '../img/Services_WALLETicon-m.png'
 
 import { SWITCH_LANGUAGE, TOGGLE_NAV } from '../actions/types';
@@ -525,47 +525,58 @@ const initialState = {
       en: [
         {
           title: "DIY Token Sale",
-          text: "Launch your own token sale with a visual interface. Remove all the worry of hackers taking advantage of bugs by using our tested and secure software. This includes the automation of transfers, cold storage or time-locked transactions, and token distribution without third parties."
+          text: "Launch your own token sale with a visual interface. Remove all the worry of hackers taking advantage of bugs by using our tested and secure software. This includes the automation of transfers, cold storage or time-locked transactions, and token distribution without third parties.",
+          img: diytoken
         },
         {
           title: "Komodo Assetchain",
-          text: "ChainZilla lets anyone create, manage, and deploy native blockchains on the Komodo blockchain with just a few clicks. No programming required. Select from our catalog of pre-configured blockchains."
+          text: "ChainZilla lets anyone create, manage, and deploy native blockchains on the Komodo blockchain with just a few clicks. No programming required. Select from our catalog of pre-configured blockchains.",
+          img: kmdicon       
         },
         {
           title: "ERC20 Token",
-          text: "ChainZilla lets anyone create, manage, and deploy smart contracts on the Ethereum blockchain with just a few clicks. No programming required. Select from our catalog of easy to configure smart contract templates."
+          text: "ChainZilla lets anyone create, manage, and deploy smart contracts on the Ethereum blockchain with just a few clicks. No programming required. Select from our catalog of easy to configure smart contract templates.",
+          img: ercicon
         },
         {
           title: "NEM Mosaics",
-          text: "ChainZilla lets anyone create, manage, and distribute NEM Mosaics and Namespace with just a few clicks. No programming required."
+          text: "ChainZilla lets anyone create, manage, and distribute NEM Mosaics and Namespace with just a few clicks. No programming required.",
+          img: nem
         },
         {
           title: "dICO APP",
-          text: "A custom application that runs user-side. It allows end-users to participate in an decentralized ICO. The dapp is powered by BarterDEX core. It’s ideal for dICOs of Bitcoin and Komodo compatibles."
+          text: "A custom application that runs user-side. It allows end-users to participate in an decentralized ICO. The dapp is powered by BarterDEX core. It’s ideal for dICOs of Bitcoin and Komodo compatibles.",
+          img: dico
         },
         {
           title: "ICO WebApp",
-          text: "A custom application that runs server-side. It allows your users to participate in your ICO through an easy to use website. It’s ideal for initial coin offerings of all types. It is one of the most secure ICO webapps available. Your funds are always in cold storage."
+          text: "A custom application that runs server-side. It allows your users to participate in your ICO through an easy to use website. It’s ideal for initial coin offerings of all types. It is one of the most secure ICO webapps available. Your funds are always in cold storage.",
+          icoicon
         },
         {
           title: "SPV Servers",
-          text: "SPV or Electrum is a technique described in Satoshi Nakamoto’s paper. SPV allows a lightweight client to verify that a transaction is included in the blockchain without downloading the entire blockchain. Also known as a Lite wallet."
+          text: "SPV or Electrum is a technique described in Satoshi Nakamoto’s paper. SPV allows a lightweight client to verify that a transaction is included in the blockchain without downloading the entire blockchain. Also known as a Lite wallet.",
+          img: spv
         },
         {
           title: "Wallets",
-          text: "Store and manage digital currencies with ease with your own custom cryptocurrency wallet."
+          text: "Store and manage digital currencies with ease with your own custom cryptocurrency wallet.",
+          img: wallet 
         },
         {
           title: "Block Explorers",
-          text: "An online blockchain browser that displays the contents of individual blocks and transactions and the transaction histories and balances."
+          text: "An online blockchain browser that displays the contents of individual blocks and transactions and the transaction histories and balances.",
+          explorer
         },
         {
           title: "E-commerce Integration",
-          text: "Boost your business sales by adding cryptocurrency payment options to your existing website. Our team of professionals will assure that you integrate the most efficient payment solution for your business."
+          text: "Boost your business sales by adding cryptocurrency payment options to your existing website. Our team of professionals will assure that you integrate the most efficient payment solution for your business.",
+          img: ecommerce
         },
         {
           title: "Cyber Security Services",
-          text: "In collaboration with DevSEC we will go back to our hacker roots. Our hackathons and bounty programs will strengthen our bonds with the white/grey-hat community and promote the their continuous growth. ChainZilla will leverage the expertise of the most capable professionals in this field to pentest, peer-review, and provide critical bug reports to secure your blockchain."
+          text: "In collaboration with DevSEC we will go back to our hacker roots. Our hackathons and bounty programs will strengthen our bonds with the white/grey-hat community and promote the their continuous growth. ChainZilla will leverage the expertise of the most capable professionals in this field to pentest, peer-review, and provide critical bug reports to secure your blockchain.",
+          img: cyber
         },
       ],
       es: [
